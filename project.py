a88991bb4e112ccd9707ff86a3fc4f4e27eb284e
dishant1123 <dishantshah1417@gmail.com>
2026-07-01 15:17:51 +0530

    ....

---
+++ project.py
@@ -0,0 +1,13 @@
+"""
+1.dataset  : mall_customers.csv 
+2. missing value , drop  , dropna 
+3. outlier  , remove  ===> capping  
+4. features selection  : spending score and  income 
+5. standardization  ===> standardscaler 
+6. elbow  method :  k extact value  
+7. EDA : plot  ===> pandas   ===> plot  
+8. heatmap  : coorelation  ===> heatmap
+9. PCA  :  principal component analysis  ===> pca
+10.scatter plot  :  pca  ===> scatter plot
+"""
+# next session  : doubt ,  deep learning

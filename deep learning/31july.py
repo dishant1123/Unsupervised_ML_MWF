2834abf742d0a6ef4d708896057e7221982db4e5
dishant1123 <dishantshah1417@gmail.com>
2026-07-31 14:46:11 +0530

    ....

---
+++ deep learning/31july.py
@@ -0,0 +1,12 @@
+"""
+
+SGD            accuracy :     loss 
+Momentum       accuracy :     loss 
+RMSprop        accuracy :     loss 
+Adam           accuracy :     loss 
+
+best optimizer : 
+
+acc high  loss low 
+
+"""

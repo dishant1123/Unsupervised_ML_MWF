f5f3db6453c6c54a618569d6e2f0fa7ff3b53e25
dishant1123 <dishantshah1417@gmail.com>
2026-08-03 15:17:19 +0530

    ....

--- deep learning/3aug.py
+++ deep learning/3aug.py
@@ -1,3 +1,15 @@
 """
 SESSION 13 - Building Neural Networks in Keras (Part 1)Topics Covered:Creating layersActivation functionsDense networksCompiling modelsTraining with fit()Training callbacksHands-on:Build a 3-layer ANN for binary classification.
 """
+"""
+
+step :1 data set  : kaggle ===> 
+step :2 data  cleaning  
+step :3 target variable
+step :4 split data
+step :5 feature scaler
+step :6 build model
+step :7 compile model
+step :8 loss ,accuracy
+
+"""
